a425664d7afe23220e9b0b5c5a2a5b455ace5a14
Dipin07 <kharayatdipin@gmail.com>
2025-03-08 15:34:24 +0530

    Integrate Api For Movies in Backend

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,9 @@
-import express from 'express';
+import express, { application } from 'express';
 import dotenv from 'dotenv';
 
 import authRoutes from './routes/auth.route.js'
+
+import movieRoutes from './routes/movie.route.js'
 import { ENV_VARS } from './config/envVars.js';
 import { connectDB } from './config/db.js';
 
@@ -14,8 +16,11 @@ const PORT = ENV_VARS.PORT
 app.use(express.json()); // will allow us to parse JSON request(req.body) bodies
 
 app.use('/api/v1/auth', authRoutes);
+app.use('/api/v1/movie', movieRoutes);
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
     connectDB()
 })
+
+
